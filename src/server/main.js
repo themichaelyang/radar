1381287dbf608f479b80d2ad0749df100d4de51f
Michael Yang <themichaelyang@gmail.com>
2016-10-01 18:12:10 -0400

    moved server code into module

--- src/server/main.js
+++ src/server/main.js
@@ -10,41 +10,10 @@ const OPTIONS = {
 const CLIENT_DIR = '/src/client';
 const PORT = 3000;
 
-let previousRequestDate;
-
-const server = require('https').createServer(OPTIONS);
-server.on('request', (request, response) => {
-  let url = CLIENT_DIR + (request.url === '/' ? '/index.html' : request.url);
-  let currentDirectory = process.cwd();
-  fs.readFile(currentDirectory + url, (error, data) => {
-    if (error) {
-      if (error.code === 'ENOENT') {
-        response.writeHead(400);
-        response.end("404: Couldn't find " + url);
-      }
-      else {
-        response.writeHead(500);
-        response.end('Error loading '+ url);
-        console.error(error);
-      }
-    }
-    else {
-      response.writeHead(200);
-      response.end(data);
-    }
-  });
-  let date = (new Date()).toString();
-  if (!previousRequestDate || previousRequestDate !== date) {
-    console.log();
-    console.log('[' + date + ']');
-  }
-  console.log(request.method + ': ' + request.url);
-  previousRequestDate = date;
-});
+let server = require('./server.js')(CLIENT_DIR, OPTIONS);
 
 server.listen(PORT);
 console.log('Listening on: ' + PORT);
-
 // http://stackoverflow.com/questions/3653065/get-local-ip-address-in-node-js
 require('dns').lookup(require('os').hostname(), (err, add, fam) => {
   console.log('http://' +add + ':' + PORT);
---
+++ src/server/server.js
@@ -0,0 +1,39 @@
+'use strict';
+
+function createServer(clientDirectory, options) {
+  let server = require('https').createServer(options);
+  let previousRequestDate;
+
+  server.on('request', (request, response) => {
+    let url = clientDirectory + (request.url === '/' ? '/index.html' : request.url);
+    let currentDirectory = process.cwd();
+    fs.readFile(currentDirectory + url, (error, data) => {
+      if (error) {
+        if (error.code === 'ENOENT') {
+          response.writeHead(400);
+          response.end("404: Couldn't find " + url);
+        }
+        else {
+          response.writeHead(500);
+          response.end('Error loading '+ url);
+          console.error(error);
+        }
+      }
+      else {
+        response.writeHead(200);
+        response.end(data);
+      }
+    });
+    let date = (new Date()).toString();
+    if (!previousRequestDate || previousRequestDate !== date) {
+      console.log();
+      console.log('[' + date + ']');
+    }
+    console.log(request.method + ': ' + request.url);
+    previousRequestDate = date;
+  });
+
+  return server;
+}
+
+module.exports = createServer;
